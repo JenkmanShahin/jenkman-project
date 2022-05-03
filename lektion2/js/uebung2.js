51833647a79dcf77518d0c9bd43b00a1c345a294
jenkman <Jenkmanshahin17@gmail.com>
2022-05-03 10:37:10 +0200

    aufgabe erledigt

--- lektion2/js/uebung2.js
+++ lektion2/js/uebung2.js
@@ -6,11 +6,11 @@
  * 1) Erstelle das Array.
  */
 function aufgabe1() {
-    console.debug("Aufgabe 1 ==========================");
+  console.debug("Aufgabe 1 ==========================");
 
-    //ToDo: füge ab hier Deinen Code ein
-    let laender = ['USA', 'Spanien', 'Frankreich', 'Deutschland', 'Japan'];
-    console.debug(laender)
+  //ToDo: füge ab hier Deinen Code ein
+  let laender = ['USA', 'Spanien', 'Frankreich', 'Deutschland', 'Japan'];
+  console.debug(laender)
 
 
 }
@@ -19,24 +19,24 @@ function aufgabe1() {
  * 2) Gebe die Laenge des Arrays aus.
  */
 function aufgabe2() {
-    console.debug("Aufgabe 2 ==========================");
+  console.debug("Aufgabe 2 ==========================");
 
-    let helden = ["Spider-Man", "Batman", "Superman"]
+  let helden = ["Spider-Man", "Batman", "Superman"]
 
-    //ToDo: füge ab hier Deinen Code in der console.debug ein
-    console.debug(helden.length)
+  //ToDo: füge ab hier Deinen Code in der console.debug ein
+  console.debug(helden.length)
 }
 
 /**
  * 3) Greife auf die Elemente zu.
  */
 function aufgabe3() {
-    console.debug("Aufgabe 3 ==========================");
+  console.debug("Aufgabe 3 ==========================");
 
-    let willkommen = ["Hallo", "Tschuess", "Mittag", "Planet", "Welt!"]
+  let willkommen = ["Hallo", "Tschuess", "Mittag", "Planet", "Welt!"]
 
-    //ToDo: füge ab hier Deinen Code ein
-    console.debug(willkommen[0], willkommen[4]);
+  //ToDo: füge ab hier Deinen Code ein
+  console.debug(willkommen[0], willkommen[4]);
 
 }
 
@@ -44,50 +44,51 @@ function aufgabe3() {
  * 4) Fuege Brot am anfang hinzu.
  */
 function aufgabe4() {
-    console.debug("Aufgabe 4 ==========================")
+  console.debug("Aufgabe 4 ==========================")
 
-    let warenkorb = ["Milch", "Eier", "Wurst"]
-    //ToDo: füge ab hier Deinen Code ein
-    warenkorb.unshift("Brot");
-    console.debug(warenkorb)
+  let warenkorb = ["Milch", "Eier", "Wurst"]
+  //ToDo: füge ab hier Deinen Code ein
+  warenkorb.unshift("Brot");
+  console.debug(warenkorb)
 }
 
 /**
  * 5) Loesche x, y und z.
  */
 function aufgabe5() {
-    console.debug("Aufgabe 5 ==========================");
+  console.debug("Aufgabe 5 ==========================");
 
-    let buchstaben = ["a", "b", "x", "y", "z", "c", "d"]
-    //ToDo: füge ab hier Deinen Code ein
-    buchstaben.splice(2, 3,);
-    console.debug(buchstaben)
+  let buchstaben = ["a", "b", "x", "y", "z", "c", "d"]
+  //ToDo: füge ab hier Deinen Code ein
+  buchstaben.splice(2, 3,);
+  console.debug(buchstaben)
 }
 
 /**
  * 6) Fuelle das Array mit Daten.
  */
 function aufgabe6() {
-    console.debug("Aufgabe 6 ==========================");
+  console.debug("Aufgabe 6 ==========================");
 
-    //ToDo: füge ab hier in das Array und die console.debugs Deinen Code ein
-    let developer = ["Jenkman", "Shahin", 28, 1.60, true]
-    console.debug("Vorname: ",developer[0])
-    console.debug("Nachname: ",developer[1] )
-    console.debug("Alter: ",developer[2] )
-    console.debug("Groesse: ", developer[3])
-    console.debug("Aufgabe erledigt: " ,developer[4] )
+  //ToDo: füge ab hier in das Array und die console.debugs Deinen Code ein
+  let developer = ["Jenkman", "Shahin", 28, 1.60, true]
+  console.debug("Vorname: ", developer[0])
+  console.debug("Nachname: ", developer[1])
+  console.debug("Alter: ", developer[2])
+  console.debug("Groesse: ", developer[3])
+  console.debug("Aufgabe erledigt: ", developer[4])
 }
 
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-    aufgabe1()
-    aufgabe2()
-    aufgabe3()
-    aufgabe4()
-    aufgabe5()
-    aufgabe6()
+  aufgabe1()
+  aufgabe2()
+  aufgabe3()
+  aufgabe4()
+  aufgabe5()
+  aufgabe6()
 }
+
 main()
